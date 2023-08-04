d2a299fac8c43eca71d7b57ab06fe38599444dc9
ppz1 <06.phas@gmail.com>
2023-08-04 15:09:08 -0300

    Initial commit

---
+++ aula.js
@@ -0,0 +1,16 @@
+// Math.eandom() gera números aleatorios
+console.log(Math.random())
+console.log(Math.random()*20)
+console.log(Math.random(7 -3 + 1)+3)
+
+// Arredonda para o valor inteiro mais próximo
+console.log(Math.round (3.1))
+console.log(Math.round (3.9))
+
+// Arredonda para baixo
+console.log(Math.floor(3.1))
+console.log(Math.floor(3.9))
+
+// Arredonda para cima 
+console.log(Math.ceil(3.1))
+console.log(Math.ceil(3.1))
